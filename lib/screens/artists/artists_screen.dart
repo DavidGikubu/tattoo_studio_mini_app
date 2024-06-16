584680945b1b3e7929495f19534c36ac011a4153
david <davidndungu8203@gmail.com>
2024-06-17 00:19:31 +0300

    this adds an artist screen that lists the artists style of tattooing

--- lib/screens/artists/artists_screen.dart
+++ lib/screens/artists/artists_screen.dart
@@ -1,1 +1,31 @@
 // TODO Implement this library.
+// lib/screens/artists/artists_screen.dart
+
+import 'package:flutter/material.dart';
+import '../../models/artist.dart'; // Import the Artist model.
+import '../../utils/image_urls.dart'; // Import the image URLs.
+import '../../widgets/artist_card.dart'; // Import the artist card widget.
+
+class ArtistsScreen extends StatelessWidget {
+  final List<Artist> artists = [
+    // Sample list of artists.
+    Artist(name: 'Alice', bio: 'Expert in modern tattoos', profileImage: artistImages[0], tattoos: []),
+    Artist(name: 'Bob', bio: 'Specializes in traditional tattoos', profileImage: artistImages[1], tattoos: []),
+    Artist(name: 'Charlie', bio: 'Best for geometric designs', profileImage: artistImages[2], tattoos: []),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Choose an Artist'),
+      ),
+      body: ListView.builder(
+        itemCount: artists.length,
+        itemBuilder: (context, index) {
+          return ArtistCard(artist: artists[index]); // Use the artist card widget.
+        },
+      ),
+    );
+  }
+}
