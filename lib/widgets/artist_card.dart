137ed2e96a520892c6c7ec8c8658ef65c3f3be81
david <davidndungu8203@gmail.com>
2024-06-17 00:32:01 +0300

    this adds artists cards to show artists profile.

--- lib/widgets/artist_card.dart
+++ lib/widgets/artist_card.dart
@@ -1,1 +1,27 @@
 // TODO Implement this library.
+import 'package:flutter/material.dart';
+import '../../models/artist.dart'; // Import the Artist model.
+import '../../screens/artists/artist_detail_screen.dart'; // Import the artist detail screen.
+
+class ArtistCard extends StatelessWidget {
+  final Artist artist;
+
+  ArtistCard({required this.artist});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage(artist.profileImage), // Display the artist's profile image.
+      ),
+      title: Text(artist.name), // Display the artist's name.
+      subtitle: Text(artist.bio), // Display the artist's bio.
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => ArtistDetailScreen(artist: artist)), // Navigate to the artist detail screen.
+        );
+      },
+    );
+  }
+}
