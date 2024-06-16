ea0bc0fbd3e1599bd16b1ad928aebe4cdc83f60f
david <davidndungu8203@gmail.com>
2024-06-17 00:06:18 +0300

    this creates a class to represent a tattoo

---
+++ lib/models/tattoo.dart
@@ -0,0 +1,12 @@
+// Defines a class to represent a Tattoo.
+class Tattoo {
+  final String title; // The title of the tattoo.
+  final String imageUrl; // URL of the tattoo image.
+  final String description; // A short description of the tattoo.
+
+  Tattoo({
+    required this.title,
+    required this.imageUrl,
+    required this.description,
+  });
+}
