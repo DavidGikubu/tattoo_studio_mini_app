be5230a5cc7d7fd8858809210d2766aa65a83092
david <davidndungu8203@gmail.com>
2024-06-17 00:29:53 +0300

    this creates a tattoo card to display the tattoo image.

--- lib/widgets/tattoo_card.dart
+++ lib/widgets/tattoo_card.dart
@@ -1,1 +1,22 @@
 // TODO Implement this library.
+import 'package:flutter/material.dart';
+import '../../models/tattoo.dart'; // Import the Tattoo model.
+
+class TattooCard extends StatelessWidget {
+  final Tattoo tattoo;
+
+  TattooCard({required this.tattoo});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: [
+          Image.network(tattoo.imageUrl), // Display the tattoo image.
+          Text(tattoo.title, style: TextStyle(fontWeight: FontWeight.bold)), // Display the tattoo title.
+          Text(tattoo.description), // Display the tattoo description.
+        ],
+      ),
+    );
+  }
+}
